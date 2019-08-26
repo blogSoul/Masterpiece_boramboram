e178e0d31df72167430d8c5e1752fe8dcf7cb3e6
blogSoul <blo9040@gmail.com>
2019-08-26 20:05:56 +0900

    base.html connect

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['myproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
