489d76314ba7ac6d03578ac5e8ef3490f1dd68dd
blogSoul <blo9040@gmail.com>
2019-08-27 17:27:43 +0900

    servicecenter connected

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import Mainscreen.views
+import servicecenter.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Mainscreen.views.home, name='home')
+    path('', Mainscreen.views.home, name='home'),
+    path('servicecenter/', servicecenter.views.home, name='servicecenter'),
+    path('servicecenter/<int:servicecenter_id>/', servicecenter.views.detail, name='detail'),
 ]
--- myproject/servicecenter/views.py
+++ myproject/servicecenter/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Servicecenter
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
-# 아직 완성 안되었습니다. 확인요망
+    notices = Servicecenter.objects
+    return render(request, 'servicecenter/home.html', {'notices': notices})
+
+def detail(request, servicecenter_id):
+    servicecenter_detail = get_object_or_404(Servicecenter, pk=servicecenter_id)
+    return render(request, 'servicecenter/detail.html', {'notice': servicecenter_detail})
