8e08936efea9af93ec7b53825174842ffc3fe5d8
blogSoul <blo9040@gmail.com>
2019-09-23 19:27:07 +0900

    phone setting

--- myproject/accounts/models.py
+++ myproject/accounts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.core.mail import send_mail
 
 # Create your models here.
 class webuser(models.Model):
@@ -10,3 +12,8 @@ class webuser(models.Model):
     user_ph1 = models.CharField(max_length=3)
     user_ph2 = models.CharField(max_length=4)
     user_ph3 = models.CharField(max_length=4)
+
+class PhoneModel(models.Model):
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}9$', message = "Phone number must be entered in the format: '+999999999', Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True) # validators should be a list
+# 전화번호 저장을 확인하는 부분!
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -125,3 +125,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '../FrontEndFile')]
+
+# 이메일 설정 환경 변수
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = "smtp.gmail.com"
+#EMAIL_HOST_USER = 'username@gmail.com'
+#EMAIL_HOST_PASSWORD = 'your-gmail-password'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
