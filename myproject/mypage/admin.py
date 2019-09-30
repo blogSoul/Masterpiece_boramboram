9d3282fda9a2e046afcffa96db5e21e6d7961ff6
blogSoul <blo9040@gmail.com>
2019-10-01 05:27:33 +0900

    다시 해야하나

--- myproject/mypage/admin.py
+++ myproject/mypage/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import mypage
+
+class ProfileInline(admin.StackedInline): # 로또 프로젝트에서 썼던 방식으로 유저 밑에 프로필 을 붙여서 보여주려고 이를 상속받음
+    model = Profile
+    con_delete = False                    # 프로필을 아예 없앨 수 없게 하는 속성(한번 만들면 지우는건 이상하니까)
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+# 기존의 User의 등록을 취소했다가 User와 ProfileInline을 붙임.
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- myproject/mypage/models.py
+++ myproject/mypage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL) # 현 계정의 사용자를 가져올 수 있음.
+    nickname = models.CharField(max_length=64)
+    profile_photo = models.ImageField(blank=True)
