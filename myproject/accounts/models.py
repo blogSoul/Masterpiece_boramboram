20f57f4c84e1c58bea3a5496cfa633d832a12e50
blogSoul <blo9040@gmail.com>
2019-10-05 03:29:09 +0900

    로그인 회원가입 구현 완료

--- myproject/accounts/models.py
+++ myproject/accounts/models.py
@@ -6,4 +6,8 @@ class myuser(AbstractUser):
     user_realname = models.CharField(max_length=15)
     user_phone_number = models.CharField(max_length=15)
     isdesigner = models.BooleanField(default=False)
+    is_art = models.BooleanField(default=False)
+
+
 #Art 부분은 조금 더 논의하고 만들겠습니다.
+
--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,35 +1,48 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
 from django.contrib import auth
+from django.contrib.auth import get_user_model
+
 # Create your views here.
-User = AbstractUser()
+
+User = get_user_model()
 
 def signup(request):
     '''
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                request.POST['username'], password=request.POST['password1'],
-                user_nickname=request.POST['nickname'],
-                user_email=request.POST['email'],
+            user =User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'],
+                user_realname=request.POST['realname'],
+                email=request.POST['email'],
                 user_phone_number=request.POST['phonenumber']
             )
-            auth.login(request, User)
-            return redirect('home')
+            if user is None:
+                auth.login(request, User)
+                return redirect('home')
+            else:
+                return redirect('home')
     return render(request, 'accounts/signup.html')
 # 왜 안되는거야 ... ㅜㅡㅜ
     '''
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.get(request.POST['username'])
+                user = User.objects.get(
+                    username=request.POST['username'],
+                    password=request.POST['password1'],
+                    user_realname=request.POST['realname'],
+                    email=request.POST['email'],
+                    user_phone_number=request.POST['phonenumber'],
+                )
                 return render(request, 'account/signup.html', {'error': 'Username has already been taken'})
             except User.DoesNotExist:
                 user = User.objects.create_user(
-                    request.POST['username'], password=request.POST['password1'],
-                    user_nickname=request.POST['nickname'],
-                    user_email=request.POST['email'],
+                    username=request.POST['username'],
+                    password=request.POST['password1'],
+                    user_realname=request.POST['realname'],
+                    email=request.POST['email'],
                     user_phone_number=request.POST['phonenumber'],
                 )
                 auth.login(request, user)
@@ -38,6 +51,7 @@ def signup(request):
             return render(request, 'accounts/signup.html', {'error': 'Passwords must match'})
     else:
         return render(request, 'accounts/signup.html')
+    # try 구문으론 odject의 doesnotexit를 읽을 수 없다.
 
 
 def login(request):
